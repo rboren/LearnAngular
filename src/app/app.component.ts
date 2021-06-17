7f9ea812900807518acd4d848db4c7b739919ce3
Rackley Boren <rackley.boren@borenstc.com>
2021-06-17 11:28:53 -0400

    Faux navigation using ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'course-project-app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string){
+    console.log("Loaded Feature: " + feature);
+    this.loadedFeature = feature;
+  }
 }
